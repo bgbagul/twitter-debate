0c3fc896f8ca837a29d1bc3d9d33b27d9b7d3c99
Chris Blair user <Chris Blair user@localhost>
2011-10-17 20:52:53 -0500

    Added functionality to choose both users and a topic

--- index.php
+++ index.php
@@ -16,61 +16,187 @@
             Twitter Debate
         </div>
 
-        <!-- 1. drop down 1 - first group
-        // 2. drop down 2 - second group
-        // 3. text box - search
-        -->
         <table>
             <tr>
 
                 <?php
-                require "tDebate.php";
-                $results = Timeline::getUserTimeline("mittromney");
-                $searchArray = array("Social Security", "Benefits", "disability", "retirement", "insurance");
+                    require "tDebate.php";
 
-                print "<th class='accountTitle'>@MittRomney</th><th class='accountTitle'>@BarackObama</th><tr/><tr><td>";
+                    function showOptionsDrop ($array, $active, $echo=true) {
+                        $string = '';
+
+                        foreach($array as $k => $v) {
+
+                            $s = ($active == $k)? ' selected="selected"' : '';
+                            $string .= '<option value="'.$k.'"'.$s.'>'.$v.'</option>'."\n";
+                        }
+
+                        if ($echo) {
+                            echo $string;
+
+                        } else {
+                            return $string;
+                        }
+                    }
+
+                    $twitterAccounts1 = array('BarackObama'=>'Barack Obama', 'THEHermanCain'=>'Herman Cain',
+                        'MittRomney'=>'Mitt Romney', 'RonPaul'=>'Ron Paul', 'RickSantorum'=>'Rick Santorum',
+                        'MicheleBachmann'=>'Michele Bachman', 'NewtGingrich'=>'Newt Gingrich',
+                        'GovGaryJohnson'=>'Gary Johnson', 'GovernorPerry'=>'Rick Perry', 'JonHuntsman'=>'Jon Huntsman');
+                    $twitterAccounts2 = array('BarackObama'=>'Barack Obama', 'THEHermanCain'=>'Herman Cain',
+                        'MittRomney'=>'Mitt Romney', 'RonPaul'=>'Ron Paul', 'RickSantorum'=>'Rick Santorum',
+                        'MicheleBachmann'=>'Michele Bachman', 'NewtGingrich'=>'Newt Gingrich',
+                        'GovGaryJohnson'=>'Gary Johnson', 'GovernorPerry'=>'Rick Perry', 'JonHuntsman'=>'Jon Huntsman');
+                    $twitterTopics = array("economy"=>"Economy", "immigration"=>"Immigration", "healthcare"=>"Health Care", "socialsecurity"=>"Social Security");
+
+                    $searchArray1 = array("Economy", "Jobs", "Unemployment", "Spend", "Stimulus",
+                        "Taxes", "Worker", "Labor", "Fed ", "Econ", "AmericanJobsAct", "WallStreet",
+                        "EconDebate", "Companies", "Company", "Salary", "Salaries", "Audit", "Money",
+                        "Wage", "Consumer", "Work", "Prosperity", "FairTax", "Tax", "Capital",
+                        "FederalReserve", "Deficit", "Business", "Investment", "Bailout", "Funds",
+                        "Rich", "Poor", "Outsource", "Poverty");
+                    $searchArray2 = array("Immigration", "Citizenship", "DreamAct", "Border", "Fence", 
+                        "E-verify", "Everify", "Arizona", "Alabama", "deportation", "USCIS",
+                        "naturalization", "resident", "customs", "security");
+                    $searchArray3 = array("Healthcare", "Health", "care", "Obamacare", "Insured",
+                        "uninsured", "access", "socialized", "medicine", "doctor", "physician",
+                        "medical", "benefits", "insurance", "Medicaid", "Medicare",
+                        "universal", "M.D.", "MD", "drugs", "sick");
+                    $searchArray4 = array("SocialSecurity", "Benefits", "disability", "SS", "retirement");
+            ?>
+
+            <div id = "containerChoose">
+                <span>
+                    <select id="twitterAccounts1" name="twitterAccounts1[]" style="margin-left:150px;">
+                        <?php showOptionsDrop($twitterAccounts1, null, true); ?>
+                    </select>
+                </span>
+
+                <span>
+                    <select id="twitterAccounts2" name="twitterAccounts2[]" style="margin-left:600px">
+                        <?php showOptionsDrop($twitterAccounts2, null, true); ?>
+                    </select>
+                </span>
+            </div>
+
+            <div>
+                <select id="twitterTopics" name="twitterTopics[]" style="margin-left:500px"
+                    onchange='topicChanged(this.value, document.getElementById("twitterAccounts1").value,
+                        document.getElementById("twitterAccounts2").value)'>
+                    <?php showOptionsDrop($twitterTopics, null, true); ?>
+                </select>
+            </div>
+
+            <?php
+
+                if (!empty($_GET['topic'])) {
+                    $twitterTopic = $_GET['topic'];
+                    $twitterUser1 = $_GET['user1'];
+                    $twitterUser2 = $_GET['user2'];
+
+                    if ($twitterTopic === "economy") {
+                        $searchArray = $searchArray1;
+
+                    } else if ($twitterTopic === "immigration") {
+                        $searchArray = $searchArray2;
+
+                    } else if ($twitterTopic === "healthcare") {
+                        $searchArray = $searchArray3;
+
+                    } else if ($twitterTopic === "socialsecurity") {
+                        $searchArray = $searchArray4;
+                    }
+
+                } else {
+                    $twitterTopic = "Economy";
+                    $twitterUser1 = "MittRomney";
+                    $twitterUser2 = "BarackObama";
+                    $searchArray = $searchArray1;
+
+                    $searchArray = $searchArray1;
+                }
+
+                print "<th class='accountTitle'>@" . $twitterUser1 . "</th><th class='accountTitle'>@" . $twitterUser2 . "</th><tr/><tr><td>";
+
+                // Makes a search for tweets
+                $results = Timeline::getUserTimeline($twitterUser1);
 
                 // This loop prints the search results nicely with the
                 // following info: from_user(who posted it),
                 // created_at(the time), and text(the tweet itself).
                 // First while for first twitter account
-                while($tweet = next($results))
-                {
-                    if (StringUtility::searchStringWithArray($tweet->text,$searchArray))
-                    {
+                while($tweet = next($results)) {
+
+                    if (StringUtility::searchStringWithArray($tweet->text,$searchArray)) {
+
                         print "<div class='tweetCell'><b>";
-                        //echo $tweet->from_user;
-                        //print "<br>";
                         echo $tweet->created_at;
                         print "</b><br>";
                         echo $tweet->text;
                         echo "<br></div>";
                     }
                 }
-
-                $results = Timeline::getUserTimeline("barackobama");
 
                 print "</td><td>";
-                // second while for second twitter account
-                while($tweet = next($results))
-                {
-                    if (StringUtility::searchStringWithArray($tweet->text,$searchArray))
-                    {
+
+                // Makes a search for tweets
+                $results = Timeline::getUserTimeline($twitterUser2);
+
+                // This loop prints the search results nicely with the
+                // following info: from_user(who posted it),
+                // created_at(the time), and text(the tweet itself).
+                // First while for first twitter account
+                while($tweet = next($results)) {
+
+                    if (StringUtility::searchStringWithArray($tweet->text,$searchArray)) {
+
                         print "<div class='tweetCell'><b>";
-                        //echo $tweet->from_user;
-                        //print "<br>";
                         echo $tweet->created_at;
                         print "</b><br>";
                         echo $tweet->text;
                         echo "<br></div>";
                     }
                 }
-
+                
                 print "</td>"
-                ?>
+            ?>
 
             </tr>
         </table>
         </span>
+
+        <script type="text/javascript">
+            function topicChanged (topicValue, user1, user2) {
+                window.location = "index.php?topic=" +
+                                topicValue + "&user1=" + user1 + "&user2=" + user2;
+                document.getElementById("twitterAccounts1").value = user1;
+                document.getElementById("twitterAccounts2").value = user2;
+            }
+
+            function gup( name )
+            {
+              name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+              var regexS = "[\\?&]"+name+"=([^&#]*)";
+              var regex = new RegExp( regexS );
+              var results = regex.exec( window.location.href );
+              if( results == null )
+                return "";
+              else
+                return results[1];
+            }
+
+            var isTopic = gup('topic');
+            if (isTopic) {
+                document.getElementById("twitterAccounts1").value = gup('user1');
+                document.getElementById("twitterAccounts2").value = gup('user2');
+                document.getElementById("twitterTopics").value = isTopic;
+
+            } else {
+                document.getElementById("twitterAccounts1").value = "MittRomney";
+                document.getElementById("twitterAccounts2").value = "BarackObama";
+                document.getElementById("twitterTopics").value = "Economy";
+            }
+
+        </script>
     </body>
 </html>
